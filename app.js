38eebdcb810326e95b5ee55c5b0861899465bfb7
brendap21 <bpna16100224@gmail.com>
2025-03-17 01:08:18 -0600

    Se agrega una nueva funcion que actualiza la lista de amigos en el challenge, y una nueva función que sortea aleatoriamente los datos de la lista.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 let amigos = [];
 
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 function agregarAmigo() {
     const input = document.getElementById('amigo');
     const nombre = input.value.trim();
@@ -25,5 +23,18 @@ function actualizarListaAmigos() {
         li.textContent = amigo;
         lista.appendChild(li);
     });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
 }
 
