d833ddc956a6e4c10ddfc150f8443a673c77d8b3
brendap21 <bpna16100224@gmail.com>
2025-03-17 01:01:33 -0600

    Se implemente un array y una funcion que incluye la lógica para almacenar los nombres de los amigos ingresados por el usuario en una lista.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+let amigos = [];
+
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = "";
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
